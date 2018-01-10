6edf6fa48a6f2e541bf7a6d5833391a99a9fcd2e
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-01-11 00:07:12 +0800

    completed all user stories for local weather project

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,19 +1,37 @@
 $(document).ready(function () {
-
+    var temperatureC;
+    var temperatureF;
+    var units = "F";
+    
     function success(pos) {
             
         var apiWeather = "http://api.openweathermap.org/data/2.5/weather?lat="+pos.coords.latitude+"&lon="+pos.coords.longitude+"&units=imperial&appid=6401d645d8148174fc14a582998bade2";
         $.getJSON(apiWeather, function(data){
            
-            var city = data.name;
-            var fTemp = data.main.temp
+            //var city = data.name;
+            temperatureF = data.main.temp ;
+            temperatureC = parseFloat((temperatureF - 32) / 1.8).toFixed(1);
             var description = data.weather[0].description;
             var humidity = data.main.humidity;
-            $("#city").html(city);
-            $("#fTemp").html(fTemp); 
-            $("#description").html(description);
-            $("#humidity").html(humidity);           
-
+            var tempChange = false;
+            
+            $("#city").html(data.name);
+            $("#temperature").html(temperatureF + "&deg;F"); 
+            $("#description").html(data.weather[0].description);
+            $("#humidity").html(data.main.humidity);
+            $("#city").html(data.name);
+            $("#temp-units").click(function(){
+                if(units == "C") {
+                    units = "F";
+                    $("#temperature").html(temperatureF + " &deg;F");                    
+                }
+                else {
+                    units = "C";
+                    $("#temperature").html(temperatureC + " &deg;C");                    
+                }
+            });
+            setBackground(temperatureF);
+    
         });
       };
       
@@ -23,28 +41,26 @@ $(document).ready(function () {
       };
       
       navigator.geolocation.getCurrentPosition(success, error);
+
+      function setBackground(temperatureF){
+          console.log(temperatureF);
+          $('body').css('background-image','url(img/weather.jpg');         
+              
+        if (temperatureF >= 80) {
+            $("#thermometer").html("<i class = 'fa fa-thermometer-three-quarters'></i>");
+        }
+        else if (temperatureF >= 50 && temperatureF <= 79){
+            $("#thermometer").html("<i class = 'fa fa-thermometer-half'></i>");
+        }
+        else if (temperatureF >= 37 && temperatureF <= 49){
+            $("#thermometer").html("<i class = 'fa fa-thermometer-quarter'></i>");
+        }
+        else if (temperatureF <= 36){
+            $("#thermometer").html("<i class = 'fa fa-thermometer-empty'></i>");
+        }
+      }
     });
 
-    /*
-     console.log('city:'+data.name);
-     console.log('fTemp:'+data.main.temp);
-    console.log('description:'+data.weather[0].description);
-    console.log('humidity:'+data.main.humidity)
 
-    var celsius = (data.main.temp - 32) * 5/9;
-    console.log('celsius:'+ celsius)
-     //var crd = pos.coords;
-        //lat = crd.latitude;
-        //long = crd.longitude;
-    $.getJSON( "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=imperial", function(data) {	// add '&units=imperial' to get U.S. measurements
-    var api = "https://fcc-weather-api.glitch.me/api/current?";
-    var apiWeather = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=6401d645d8148174fc14a582998bade2";
-    $.getJSON(apiWeather, function(data){
-        console.log(data.coord.lat)
-    });
-        console.log('Your current position is:');
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
-        console.log(`temp: ${pos.main}`)
-        console.log(`More or less ${crd.accuracy} meters.`);
-        */
+ 
+
